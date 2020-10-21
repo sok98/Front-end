760a3abf39eb024f3330b5ab144c45a85b2e4c44
yeseul <sok98@naver.com>
2020-10-21 20:50:07 +0900

    ys admin4 axios 연결(test 전)

--- src/Page/container/AdminPage/Admin3Page.js
+++ src/Page/container/AdminPage/Admin3Page.js
@@ -5,19 +5,35 @@ import Navi from "../../component/Navi.js";
 
 import "./Admin2Page.css";
 
-class Admin3Page extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <Navi />
-        <div class="AdminPage">
-          <p>Admin3페이지</p>
-          <p>변환 처리 중입니다 ...</p>
-          <p>변환 처리가 완료되면 자동으로 다음 화면으로 넘어갑니다.</p>
-        </div>
+function Admin3Page(props) {
+  const videoid = props.match.params.videoid;
+  let body = {
+    videoid: videoid,
+    subtitle: csvfile,
+  };
+  useEffect(() => {
+    axios
+      .post("http://localhost:5050/api/admin/change", body)
+      .then((response) => {
+        console.log("axios 들어옴");
+        console.log(response);
+        alert(response);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <Navi />
+      <div class="AdminPage">
+        <br />
+        <br />
+        <h1 id="text1">변환 처리 중입니다.</h1>
+        <p id="text2">
+          변환 처리가 완료되면 자동으로 다음 화면으로 넘어갑니다.
+        </p>
       </div>
-    );
-  }
+    </div>
+  );
 }
 export default Admin3Page;
