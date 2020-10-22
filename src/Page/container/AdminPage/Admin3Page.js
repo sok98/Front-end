a93c3049fdc959e6b0b982734001294c9359a83b
yeseul <sok98@naver.com>
2020-10-22 23:32:41 +0900

    ys admin4 js 구성

--- src/Page/container/AdminPage/Admin3Page.js
+++ src/Page/container/AdminPage/Admin3Page.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 import Header from "../../component/Header.js";
 import Navi from "../../component/Navi.js";
@@ -9,7 +10,7 @@ function Admin3Page(props) {
   const videoid = props.match.params.videoid;
   let body = {
     videoid: videoid,
-    subtitle: csvfile,
+    subtitle: "", //csvfile,
   };
   useEffect(() => {
     axios
@@ -18,6 +19,7 @@ function Admin3Page(props) {
         console.log("axios 들어옴");
         console.log(response);
         alert(response);
+        window.location.pathname = "/Admin4/" + videoid;
       });
   }, []);
 
--- src/Page/container/AdminPage/Admin4Page.js
+++ src/Page/container/AdminPage/Admin4Page.js
@@ -1,20 +1,51 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import Header from "../../component/Header.js";
 import Navi from "../../component/Navi.js";
 
-class Admin4Page extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <Navi />
-        <div class="AdminPage">
-          <p>Admin4페이지</p>
-          <p>영상 최종 확인</p>
-        </div>
+function Admin4Page(props) {
+  const videoid = props.match.params.videoid;
+  const [Video, setVideo] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5050/api/admin/change" + videoid)
+      .then((response) => {
+        console.log("axios 들어옴");
+        console.log(response.data);
+        if (response.data) {
+          setVideo(response.data);
+          console.log("setVido 확인");
+          console.log(Video);
+        } else {
+          alert("Failed to get video");
+        }
+      });
+  }, []);
+
+  const submitHandler = () => {
+    axios
+      .get("http://localhost:5050/api/admin/change" + videoid)
+      .then((response) => {
+        console.log("axios 들어옴");
+        console.log(response);
+        alert(response);
+        window.location.pathname = "/Admin";
+      });
+  };
+
+  return (
+    <div>
+      <Header />
+      <Navi />
+      <div class="AdminPage">
+        <p>영상 최종 확인</p>
+        <video src={`${Video.videolink}`} controls></video>
+        <button onClick={submitHandler}>확인</button>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 export default Admin4Page;
