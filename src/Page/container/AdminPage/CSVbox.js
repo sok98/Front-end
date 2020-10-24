de6ae45cb2345cf7c0c142a8ca95b80544d62a38
yeseul <sok98@naver.com>
2020-10-24 22:53:18 +0900

    ys CSVbox 파일 업로드 시도중

--- src/Page/container/AdminPage/CSVbox.js
+++ src/Page/container/AdminPage/CSVbox.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { FileReader, useEffect, useState } from "react";
+import CSVReader from "react-csv-reader";
 
 function CSVbox(props) {
+  const [FilePath, setFilePath] = useState(null);
+  console.log("CSVbox 들어옴");
+  /*useEffect(() => {
+    reader = new FileReader();
+    reader.readAsText(props.csvfile, "UTF-8");
+    reader.onload = function () {
+      output = reader.result;
+      console.log(output);
+    };
+  }, []);
+  console.log(output);*/
+
+  const onCSVChange = (e) => {
+    setFilePath(e.target.files[0]);
+    console.log("파일 업로드");
+  };
+  console.log(FilePath);
   return (
     <div>
       <div>csv파일 읽어서</div>
       <div>테이블 형태로 반환</div>
       <div>{props.csvfile}</div>
+      {/*<div>{output}</div>*/}
+      <input type="file" onChange={onCSVChange} />
+      <textarea>{FilePath}</textarea>
     </div>
   );
 }
